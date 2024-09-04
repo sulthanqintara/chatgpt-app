1715c1c84b7c1bd41d7b00e0512303e5e0934183
sulthanqintara <sulthanqintara@gmail.com>
2024-09-04 13:17:29 +0700

    Layout

---
+++ src/app/about/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function ABoutRoute() {
+    return (
+        <main>
+            <h1 className="text-4xl font-bold">About Us</h1>
+            <div className="mt-5">About Us And Our Awesome Application</div>
+        </main>
+    );
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,35 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "NextJS ChatGPT App",
-  description: "Brought to you by NextJS and ChatGPT",
+    title: "NextJS ChatGPT App",
+    description: "Brought to you by NextJS and ChatGPT",
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <body className={`${inter.className} px-2 md:px-5`}>
+                <header className="text-white font-bold bg-green-900 text-2xl p-2 mb-3 rounded-b-lg shadow-gray-700 shadow-lg flex">
+                    <div className="flex flex-grow">
+                        <Link href="/">GPT Chat</Link>
+                        <Link href="/about" className="ml-5 font-light">
+                            About
+                        </Link>
+                    </div>
+                </header>
+                <div className="flex flex-col md:flex-row">
+                    <div className="flex-grow">{children}</div>
+                </div>
+            </body>
+        </html>
+    );
 }
