c26521cf28f8d9fc991b76ac305cd1deb42ed69a
sulthanqintara <sulthanqintara@gmail.com>
2024-09-27 14:15:52 +0700

    auth

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,13 +1,18 @@
-import NextAuth from "next-auth";
+import NextAuth, { CallbacksOptions } from "next-auth";
 import GitHubProvider from "next-auth/providers/github";
 
 const authOptions = {
-    providers: [
-        GitHubProvider({
-            clientId: process.env.GITHUB_ID ?? "",
-            clientSecret: process.env.GITHUB_SECRET ?? "",
-        }),
-    ],
+  callbacks: {
+    async signIn({ profile }: { profile: { login: string } }) {
+      return profile.login === "sulthanqintara";
+    },
+  } as unknown as CallbacksOptions,
+  providers: [
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID ?? "",
+      clientSecret: process.env.GITHUB_SECRET ?? "",
+    }),
+  ],
 };
 
 const handler = NextAuth(authOptions);
