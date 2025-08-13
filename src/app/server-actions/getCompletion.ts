e83350dd3de60e2b78f23af13b52b724a8a98a6f
Sulthan Qintara <sulthanqintara@gmail.com>
2025-08-13 16:34:10 +0700

    add chatgpt functionality, but I have no creds :(

---
+++ src/app/server-actions/getCompletion.ts
@@ -0,0 +1,29 @@
+"use server";
+import OpenAI from "openai";
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+export async function getCompletion(
+  messageHistory: {
+    role: "user" | "assistant";
+    content: string;
+  }[]
+) {
+  console.log(process.env.OPENAI_API_KEY, "key");
+  const response = await openai.chat.completions.create({
+    model: "gpt-3.5-turbo",
+    messages: messageHistory,
+  });
+
+  const messages = [
+    ...messageHistory,
+    response.choices[0].message as unknown as {
+      role: "user" | "assistant";
+      content: string;
+    },
+  ];
+
+  return { messages };
+}
